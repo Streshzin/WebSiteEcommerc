716deae6ca39d464b3aff0c45f9b344f902a862c
Streshzin <hddanilosilva@gmail.com>
2023-09-15 13:52:05 -0300

    ;ok

--- js/main.js
+++ js/main.js
@@ -2,10 +2,36 @@
 let cartIcon = document.querySelector('#cart-icon');
 let cart = document.querySelector('.cart');
 let closeCart = document.querySelector('#close-cart');
-
+// Open Cart
 cartIcon.onclick = () => {
     cart.classList.add("active")
 };
+// Close Cart
 closeCart.onclick = () => {
     cart.classList.remove("active")
 };
+
+
+// Cart Working JS
+if (document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready();
+}
+
+// Making Function
+function ready(){
+    // Remove Items From Cart
+    var reomveCartButtons = document.getElementsByClassName("cart-remove");
+    console.log(reomveCartButtons)
+    for (var i = 0; i < reomveCartButtons.length; i++) {
+      var button = reomveCartButtons[i];
+      button.addEventListener("click", removeCartItem); 
+    }
+}
+
+// Reomve Items From Cart
+function removeCartItem(event) {
+    var buttonClicked = event.target;
+    button.Clicked.parentELement.remove();
+}
