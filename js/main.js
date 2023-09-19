6bc83452ded44bdf2293766d75cefd8ea522622f
Streshzin <hddanilosilva@gmail.com>
2023-09-19 12:24:13 -0300

    att

--- js/main.js
+++ js/main.js
@@ -21,48 +21,78 @@ if (document.readyState == 'loading') {
 // Making Function
 function ready(){
 // Reomve Items From Cart
-var reomveCartButtons = document.getElementsByClassName('cart-remove')
-console.log(reomveCartButtons)
+var reomveCartButtons = document.getElementsByClassName('cart-remove');
+//console.log(reomveCartButtons);
 for (var i = 0; i < reomveCartButtons.length; i++){
-    var button = reomveCartButtons[i]
-    button.addEventListener('click', removeCartItem)
+    var button = reomveCartButtons[i];
+    button.addEventListener('click', removeCartItem);
   }
   // Quantity Changes
-  var quantityInputs = document.getElementsByClassName('cart-quantity')
+  var quantityInputs = document.getElementsByClassName("cart-quantity");
   for (var i = 0; i < quantityInputs.length; i++){
     var input = quantityInputs[i]
-    input.addEventListener('change', quantityChanged)
+    input.addEventListener("change", quantityChanged);
+  }
+  // Add to Cart
+  var addCart = document.getElementsByClassName("add-cart");
+  for (var i = 0; i < addCart.length; i++) {
+    var button = addCart[i];
+    button.addEventListener('clcik', addCartClicked);
   }
 }
 
 // Reomve Items From Cart
 function removeCartItem(event){
-    var buttonClicked = event.target
-    buttonClicked.parentElement.remove()
-    updatetotal()
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+    updatetotal();
 }
 // Quantity Changes
-function quantityChanged(event) {
-    var input = event.target
-    if (isNaN(input.value) || input.value >= 0) {
-        input.value = 1
+function quantityChanged(event){
+    var input = event.target;
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
     }
     updatetotal();
 }
+// Add To Cart
+function addCartClicked(event) {
+    var button = event.target;
+    var shopProducts = button.parentElement;
+    var titulo = shopProducts.getElementsByClassName("product-titulo")[0].innerText;
+    var price = shopProducts.getElementsByClassName("price")[0].innerText;
+    var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+    addProductToCart(titulo, price, productImg);
+    updatetotal();
+}
+function addProductToCart(titulo, price, productImg) {
+    var cartShopBox = document.createElement("div")
+    // cartShopBox.classList.add("cart-box")
+    var cartItems = document.getElementsByClassName("cart-content")[0]
+    var cartItemsNames = cartItems.getElementsByClassName("cart-product-titulo");
+    for (var i = 0; i < cartItemsNames.length; i++) {
+        alert("You have alread add this item to cart");
+    }
+}
+
+
+
 
 // Update Total
-function updatetotal(){
-    var cartContent = document.getElementsByClassName('cart-content')[0]
-    var cartBoxes = cartContent.getElementsByClassName('cart-box')
-    var  total = 0
-    for (var i = 0; i < cartBoxes.length; i++){
-        var cartBox = cartBoxes[i]
-        var priceElement = cartBox.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0]
-        var price = parseFloat(priceElement.innerText.replace('R$', ''))
-        var quantity = quantityElement.value
-        total = total + (price * quantity)
+function updatetotal() {
+    var cartContent = document.getElementsByClassName("cart-content")[0];
+    var cartBoxes = cartContent.getElementsByClassName("cart-box");
+    var total = 0;
+    for (var i = 0; i < cartBoxes.length; i++) {
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+        var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+        var price = parseFloat(priceElement.innerText.replace("$", ""));
+        var quantity = quantityElement.value;
+        total = total + (price * quantity);
+        // If price Contain some Cents Value
+        total = Math.round(total *100) / 100
 
-        document.getElementsByClassName('total-price')[0].innerText = 'R$' + total;
+        document.getElementsByClassName("total-price")[0].innerText = "$" + total;
     }
 }
